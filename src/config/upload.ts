130db2d1b72afc3ee3e2001f2e6d45db46cbc8dc
Antony Lemos <lemosantony@gmail.com>
2020-05-14 09:13:41 -0300

    🗃️ storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,13 +2,14 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
-const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tempFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
   storage: multer.diskStorage({
-    destination: tempFolder,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('HEX');
       const fileName = `${fileHash}-${file.originalname}`;
