f648a06c58d7e527610fec1bac30665cf6b38c09
Antony Lemos <lemosantony@gmail.com>
2020-05-18 13:49:04 -0300

    🔑 password recovery

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,25 @@
+import { uuid } from 'uuidv4';
+
+import UserToken from '../../infra/typeorm/entities/UserToken';
+
+import IUserTokensRepository from '../IUserTokensRepository';
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export default FakeUserTokensRepository;
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,7 +2,9 @@ import { injectable, inject } from 'tsyringe';
 
 // import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 // import User from '../infra/typeorm/entities/User';
 
@@ -16,11 +18,22 @@ class SendForgotPassowrdEmailService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('IMailProvider')
+    @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User dos not exists.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido.',
