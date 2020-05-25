a4db9875c18d7e55fbf7a9b5361c13e465b39004
Antony Lemos <lemosantony@gmail.com>
2020-05-25 20:27:13 -0300

    📅 filter appointments per month

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,44 @@
+import { injectable, inject } from 'tsyringe';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+      },
+    );
+
+    console.log(appointments);
+
+    return [];
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
